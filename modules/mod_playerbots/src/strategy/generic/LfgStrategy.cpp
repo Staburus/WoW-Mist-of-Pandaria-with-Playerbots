3f780eef95381eefd9108b928c9c11fe5c343b86
Staburus <mdtbolt01@gmail.com>
2026-06-28 03:42:58 -0400

    Update LfgStrategy.cpp

--- modules/mod_playerbots/src/strategy/generic/LfgStrategy.cpp
+++ modules/mod_playerbots/src/strategy/generic/LfgStrategy.cpp
@@ -4,7 +4,7 @@
 
 void LfgStrategy::InitTriggers(std::vector<TriggerNode*>& triggers)
 {
-    triggers.push_back(new TriggerNode("lfg join timer", NextAction::array(0, new NextAction("lfg join", relevance), nullptr)));
+    triggers.push_back(new TriggerNode("often", NextAction::array(0, new NextAction("lfg join", relevance), nullptr)));
     triggers.push_back(new TriggerNode("seldom", NextAction::array(0, new NextAction("lfg leave", relevance), nullptr)));
 }
 
